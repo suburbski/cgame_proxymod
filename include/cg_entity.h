8dc1177d43ad5584d58b7605f3df54be8e1ed564
jvandeweyer <jelle.vandeweyer@scarlet.be>
2019-11-10 14:40:08 +0100

    Rename include guard and cvar.

--- include/cg_entity.h
+++ include/cg_entity.h
@@ -1,5 +1,5 @@
-#ifndef CG_SOUND_H
-#define CG_SOUND_H
+#ifndef CG_ENTITY_H
+#define CG_ENTITY_H
 
 #include <stdint.h>
 
@@ -9,4 +9,4 @@ void update_entityStates(void);
 
 int8_t should_filter_sound(int entity_num, int8_t is_loop);
 
-#endif // CG_SOUND_H
+#endif // CG_ENTITY_H
--- src/cg_entity.c
+++ src/cg_entity.c
@@ -1,12 +1,11 @@
 #include "cg_entity.h"
 
 #include "cg_cvar.h"
-#include "cg_local.h"
 #include "cg_utils.h"
 
-static vmCvar_t mdd_local_sounds_only;
+static vmCvar_t sound_local_only;
 
-static cvarTable_t sound_cvars[] = { { &mdd_local_sounds_only, "mdd_local_sounds_only", "0", CVAR_ARCHIVE } };
+static cvarTable_t sound_cvars[] = { { &sound_local_only, "mdd_sound_local_only", "0", CVAR_ARCHIVE } };
 
 static entityState_t cg_entityStates[1024];
 
@@ -29,14 +28,14 @@ int8_t should_filter_sound(int entity_num, int8_t is_loop)
 {
   update_cvars(sound_cvars, ARRAY_LEN(sound_cvars));
 
-  // todo: also bail if its sp?
-  if (!mdd_local_sounds_only.integer) return 0;
+  // TODO: also bail if its sp?
+  if (!sound_local_only.integer) return 0;
 
   // this messes up our own weapon noises
   // can't do anything about this w/o df code
   if (!is_loop && entity_num == ENTITYNUM_WORLD) return 1;
 
-  // refers to a entity number we dont have (ie. ourself!)
+  // refers to a entity number we dont have (i.e. ourself!)
   // no clue whose sound it could be just let it play
   if (cg_entityStates[entity_num].number == -1) return 0;
 
