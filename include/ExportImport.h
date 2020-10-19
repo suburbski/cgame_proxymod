3c6857d8ce47bdc569c82194ec8e06c07de418d5
jvandeweyer <jelle.vandeweyer@scarlet.be>
2020-10-20 00:02:15 +0200

    Resolve gcc 64-bit unused cdecl warning.

--- include/ExportImport.h
+++ include/ExportImport.h
@@ -15,17 +15,27 @@
 #if defined(_MSC_VER)
 #  define EXPORT __declspec(dllexport)
 #  define IMPORT __declspec(dllimport)
-#  define QDECL __cdecl
-#else
+#elif defined(__GNUC__) || defined(__clang__)
 #  define EXPORT __attribute__((visibility("default")))
 #  define IMPORT
-#  if defined(__GNUC__)
-#    define QDECL __attribute__((__cdecl__))
-#  elif defined(__clang__)
-#    define QDECL __cdecl
+#else
+#  error Unsupported compiler.
+#endif
+
+#if defined(_MSC_VER)
+#  define QDECL __cdecl
+#elif defined(__clang__)
+#  define QDECL __attribute__((cdecl))
+#elif defined(__GNUC__)
+#  if defined(__x86_64__)
+#    define QDECL // warning: 'cdecl' attribute ignored [-Wattributes]
+#  elif defined(__i386__)
+#    define QDECL __attribute__((cdecl))
 #  else
-#    error Unsupported compiler.
+#    error Unsupported architecture.
 #  endif
+#else
+#  error Unsupported compiler.
 #endif
 
 #endif // IMPORTEXPORT_H
